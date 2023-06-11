1c0eae6dae8fad042e118004cf8fcf776c5b41c3
Thaj Anaz <anaz.thaj@gmail.com>
2023-06-11 19:28:40 +0530

    add: import connectDB from config/db.js

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,10 @@ import express from "express";
 import dotenv from "dotenv";
 import userRoutes from "./routes/userRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import connectDB from "./config/db.js";
 
 dotenv.config();
+connectDB();
 
 const port = process.env.PORT || 5000;
 const app = express();
