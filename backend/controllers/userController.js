5ddc8d9463a59c5d5894e106b953ae6afc907dcd
Thaj Anaz <anaz.thaj@gmail.com>
2023-06-12 18:11:03 +0530

    feat: getUser & updateUser

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -68,14 +68,39 @@ const logoutUser = asyncHandler(async (req, res) => {
 // route      GET /api/users/profile
 // @access    Private
 const getUserProfile = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Get User Profile" });
+  const user = {
+    _id: req.user._id,
+    name: req.user.name,
+    email: req.user.email,
+  };
+  res.status(200).json(user);
 });
 
 // @desc      Update user profile
 // route      PUT /api/users/profile
 // @access    Private
 const updateUserProfile = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Update User Profile" });
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email | user.email;
+
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+
+    const updateUser = await user.save();
+
+    res.status(200).json({
+      _id: updateUser._id,
+      name: updateUser.name,
+      email: updateUser.email,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
 });
 
 export {
