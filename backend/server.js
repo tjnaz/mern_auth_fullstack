ae2ce01053c69b3eb252795b1d51564e1681d4e1
Thaj Anaz <anaz.thaj@gmail.com>
2023-06-12 12:13:41 +0530

    add: cookie-parser import

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+//
 import userRoutes from "./routes/userRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import connectDB from "./config/db.js";
@@ -12,6 +14,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 app.use("/api/users", userRoutes);
 app.get("/", (req, res) => res.send("Server is ready"));
 app.use(notFound);
