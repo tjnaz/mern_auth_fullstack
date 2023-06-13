65e3bf4ad1793c4a7995651711980fd2177227cb
Thaj Anaz <anaz.thaj@gmail.com>
2023-06-13 16:37:54 +0530

    feat: hero component for authentication card

---
+++ frontend/src/components/Hero.jsx
@@ -0,0 +1,29 @@
+import { Container, Card, Button } from "react-bootstrap";
+
+const Hero = () => {
+  return (
+    <div className="py-5">
+      <Container className="d-flex justify-content-center">
+        <Card className="p-5 d-flex flex-column align-items-center hero-card bg-light w-75">
+          <h1 className="text-center mb-4">MERN Authentication</h1>
+          <p className="text-center mb-4">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta vel
+            repellat quisquam temporibus voluptate quidem iusto dolorem animi
+            vero voluptatem cupiditate et enim iure commodi laborum neque
+            delectus, magni nam?
+          </p>
+          <div className="d-flex">
+            <Button variant="primary" href="/login" className="me-3">
+              Sign In
+            </Button>
+            <Button variant="primary" href="/register">
+              Sign Up
+            </Button>
+          </div>
+        </Card>
+      </Container>
+    </div>
+  );
+};
+
+export default Hero;
