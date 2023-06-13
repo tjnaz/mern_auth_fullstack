6af0ef79632c8ab985a80cca80821d9add3a97d6
Thaj Anaz <anaz.thaj@gmail.com>
2023-06-13 18:43:34 +0530

    feat: login and the register screens

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -9,11 +9,15 @@ import {
 } from "react-router-dom";
 import App from "./App.jsx";
 import HomeScreen from "./screens/HomeScreen";
+import LoginScreen from "./screens/LoginScreen";
+import RegisterScreen from "./screens/RegisterScreen";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route index={true} path="/" element={<HomeScreen />} />
+      <Route path="/login" element={<LoginScreen />} />
+      <Route path="/register" element={<RegisterScreen />} />
     </Route>
   )
 );
