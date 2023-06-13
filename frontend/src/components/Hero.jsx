5375f4b00ed16333621024cbfc4d956883632754
Thaj Anaz <anaz.thaj@gmail.com>
2023-06-13 18:12:57 +0530

    add: replace href with linkcontainer

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,4 +1,5 @@
 import { Container, Card, Button } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 
 const Hero = () => {
   return (
@@ -6,19 +7,24 @@ const Hero = () => {
       <Container className="d-flex justify-content-center">
         <Card className="p-5 d-flex flex-column align-items-center hero-card bg-light w-75">
           <h1 className="text-center mb-4">MERN Authentication</h1>
+
           <p className="text-center mb-4">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta vel
             repellat quisquam temporibus voluptate quidem iusto dolorem animi
             vero voluptatem cupiditate et enim iure commodi laborum neque
             delectus, magni nam?
           </p>
+
           <div className="d-flex">
-            <Button variant="primary" href="/login" className="me-3">
-              Sign In
-            </Button>
-            <Button variant="secondary" href="/register">
-              Sign Up
-            </Button>
+            <LinkContainer to="/login">
+              <Button variant="primary" className="me-3">
+                Sign In
+              </Button>
+            </LinkContainer>
+
+            <LinkContainer to="/register">
+              <Button variant="secondary">Sign Up</Button>
+            </LinkContainer>
           </div>
         </Card>
       </Container>
