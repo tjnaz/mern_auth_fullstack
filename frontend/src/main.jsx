ce0820d3cefa9c6bd849b66b72060d4df41c49a2
Thaj Anaz <anaz.thaj@gmail.com>
2023-06-13 16:56:53 +0530

    feat: usage of router from react-router-dom

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,14 +3,23 @@ import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
 import {
   createBrowserRouter,
-  createRouteFromElements,
+  createRoutesFromElements,
   Route,
   RouterProvider,
 } from "react-router-dom";
 import App from "./App.jsx";
+import HomeScreen from "./screens/HomeScreen";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index={true} path="/" element={<HomeScreen />} />
+    </Route>
+  )
+);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
