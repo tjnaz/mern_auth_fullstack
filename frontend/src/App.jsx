bd79b1a42e170f40b4234ce733992b662857d6c4
Thaj Anaz <anaz.thaj@gmail.com>
2023-06-13 16:27:28 +0530

    add: import homescreen

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import Header from "./components/Header";
+import HomeScreen from "./screens/HomeScreen";
 
 const App = () => {
   return (
     <>
       <Header />
-      <h1>MERN AUTH</h1>
+      <HomeScreen />
     </>
   );
 };
