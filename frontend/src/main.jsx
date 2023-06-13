23b119d53c8c679784437cffaa04f992fddf980c
Thaj Anaz <anaz.thaj@gmail.com>
2023-06-13 16:45:17 +0530

    add: import components from react-router-dom

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
+import {
+  createBrowserRouter,
+  createRouteFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
