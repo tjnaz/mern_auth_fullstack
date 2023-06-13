9d03ee606712ff144e4c3fa724b1013a55736f27
Thaj Anaz <anaz.thaj@gmail.com>
2023-06-13 18:05:21 +0530

    add: import container from react-bootstrap

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,14 @@
 import { Outlet } from "react-router-dom";
+import { Container } from "react-bootstrap";
 import Header from "./components/Header";
 
 const App = () => {
   return (
     <>
       <Header />
-      <Outlet />
+      <Container className="my-2">
+        <Outlet />
+      </Container>
     </>
   );
 };
